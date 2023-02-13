9545c1362d02adee4d86c7a60b04c5a68f0b7d58
seonghoo1217 <seonghoo1217@naver.com>
2023-02-13 22:18:00 +0900

    style : logging git oauth code

--- src/main/java/com/example/oauthreacttest/controller/CallBackController.java
+++ src/main/java/com/example/oauthreacttest/controller/CallBackController.java
@@ -2,6 +2,8 @@ package com.example.oauthreacttest.controller;
 
 import com.example.oauthreacttest.service.GitHubSocialService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
+@Log4j2
 public class CallBackController {
 
 	private final GitHubSocialService gitHubSocialService;
@@ -25,6 +28,7 @@ public class CallBackController {
 
 	@PostMapping ("/callback/github")
 	public ResponseEntity<?> getLogin(@PathParam("code") String code) throws IOException {
+		log.info("git code="+code);
 		String accessToken = gitHubSocialService.getAccessToken(code);
 		return new ResponseEntity<>(accessToken, HttpStatus.OK);
 	}
